a81132b060bf22832487769d20f01713372a0ab3
adriton <adrtler@gmail.com>
2021-01-01 18:07:12 +0100

    Pantalla Escritorio

--- src/pages/Ingresar.js
+++ src/pages/Ingresar.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Form, Input, Button, InputNumber } from 'antd';
+import { Form, Input, Button, InputNumber, Typography, Divider } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router-dom';
 
+const { Title, Text } = Typography;
 
 const layout = {
     labelCol: { span: 8 },
@@ -12,44 +14,53 @@ const tailLayout = {
 };
 
 export const Ingresar = () => {
+
+    const history = useHistory();
+
     const onFinish = values => {
         console.log('Success:', values);
+        history.push('/escritorio');
     };
 
     const onFinishFailed = errorInfo => {
         console.log('Failed:', errorInfo);
     };
     return (
-        <Form
-            {...layout}
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-        >
-            <Form.Item
-                label="Nombre del agente"
-                name="agente"
-                rules={[{ required: true, message: 'Por favor, ingrese su nombre' }]}
+        <>
+            <Title level={2}>Ingresar</Title>
+            <Text>Ingrese su nombre y número de escritorio</Text>
+            <Divider></Divider>
+            <Form
+                {...layout}
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
             >
-                <Input />
-            </Form.Item>
+                <Form.Item
+                    label="Nombre del agente"
+                    name="agente"
+                    rules={[{ required: true, message: 'Por favor, ingrese su nombre' }]}
+                >
+                    <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Escritorio"
-                name="escritorio"
-                rules={[{ required: true, message: 'Ingrese el número de escritorio' }]}
-            >
-                <InputNumber min={1} max={99} />
-            </Form.Item>
+                <Form.Item
+                    label="Escritorio"
+                    name="escritorio"
+                    rules={[{ required: true, message: 'Ingrese el número de escritorio' }]}
+                >
+                    <InputNumber min={1} max={99} />
+                </Form.Item>
 
 
-            <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit" shape="round">
-                    <SaveOutlined />
-                    Ingresar
-        </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit" shape="round">
+                        <SaveOutlined />
+                        <span>Ingresar</span>
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     )
 }
