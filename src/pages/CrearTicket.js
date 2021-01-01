ca8194a538491e55f17188f5a208cd6ac7b340eb
adriton <adrtler@gmail.com>
2021-01-01 18:19:35 +0100

    Pantalla crear Ticket

--- src/pages/CrearTicket.js
+++ src/pages/CrearTicket.js
@@ -1,9 +1,13 @@
-import { Col, Row, Typography } from 'antd';
+import { Button, Col, Row, Typography } from 'antd';
 import React from 'react';
+import { DownloadOutlined } from '@ant-design/icons';
 
 const { Title, Text } = Typography;
 
 export const CrearTicket = () => {
+  const nuevoTicket = () => {
+    console.log('Nuevo Ticket');
+  };
   return (
     <>
       <Row>
@@ -11,6 +15,24 @@ export const CrearTicket = () => {
           <Title level={3}>
             Presione el botón para generar un nuevo ticket
           </Title>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+            size="large"
+            onClick={nuevoTicket}
+          >
+            Nuevo Ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Su número</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            55
+          </Text>
         </Col>
       </Row>
     </>
